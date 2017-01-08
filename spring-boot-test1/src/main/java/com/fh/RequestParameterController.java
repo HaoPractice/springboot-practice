c6f54366b5fd69e95f63bcc57454e726e142d0fb
hao19 <hao199209@gmail.com>
2017-01-08 17:54:11 +0800

    获取请求中的字段
    获取请求中的字段demo

--- spring-boot-test1/src/main/java/com/fh/RequestParameterController.java
+++ spring-boot-test1/src/main/java/com/fh/RequestParameterController.java
@@ -2,32 +2,38 @@ package com.fh;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("hello")
+@RestController("helloParam")
+@RequestMapping("test/param")
 public class RequestParameterController {
 
 	@Autowired
 	DataBaseConfig dataBaseConfig;
 	
-	@ResponseBody
-	@GetMapping("/hello")
-	String home() {
-	  return dataBaseConfig.getAddress()+dataBaseConfig.getContent();
-	}
-	
     @ResponseBody
-    @GetMapping("/helloGet")
-    String helloGet() {
-        return "helloGet";
+    @GetMapping("/helloGet/{pathParam}")
+    String helloGet(@PathVariable(name="pathParam")String pathParam,@RequestParam("param")String requestParam) {
+        return "pathParam:"+ pathParam + ",param:"+requestParam;
     }
     
     @ResponseBody
-    @PostMapping("/helloPost")
-    String helloPost() {
-      return "helloPost";
+    @PostMapping("/helloPost/{name}/id")
+    String helloPost(@PathVariable(name="name") String string) {
+      return "name is "+ string;
     }
-
+    
+    @ResponseBody
+    @PostMapping("/helloPost/{id}/user")
+    String getUserProfile(@PathVariable(name="id") String string,@RequestParam(defaultValue="default") String profile) {
+      return "Id" + string +"profile data is "+ profile;
+    }
+    
+    
+    
 }
