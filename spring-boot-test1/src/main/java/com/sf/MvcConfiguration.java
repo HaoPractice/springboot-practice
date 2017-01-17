141e1b45be89a2c063fe3af95d80c1a298a390ef
hao19 <hao199209@gmail.com>
2017-01-18 00:01:20 +0800

    使用bean的方式配置。未让他生效
    未生效

---
+++ spring-boot-test1/src/main/java/com/sf/MvcConfiguration.java
@@ -0,0 +1,25 @@
+package com.sf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//@Configuration
+//@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
