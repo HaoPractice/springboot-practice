53080bb0456bf15114e09e1ea40ec45713433bae
hao19 <hao199209@gmail.com>
2017-01-18 00:05:28 +0800

    并没起作用的几个文件
    spring boot并不推荐xml配置
    后期改为使用properties配置

--- spring-boot-test1/src/main/java/com/sf/SpringBootRunner.java
+++ spring-boot-test1/src/main/java/com/sf/SpringBootRunner.java
@@ -1,14 +1,22 @@
-package com.fh;
+package com.sf;
 
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@SpringBootApplication  
+@SpringBootApplication
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class SpringBootRunner {
 
     public static void main(String[] args) throws Exception {
        SpringApplication.run(SpringBootRunner.class, args);
+    }
+    @Test
+    public void testRun(){
+      SpringApplication.run(SpringBootRunner.class);
     }
 }
